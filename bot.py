0df10ab44b74fa251025a4bc450f3463524b5d16
Fawwaz Thoerif <fawwazthoerif@gmail.com>
2024-07-01 19:42:51 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -36,9 +36,7 @@ class Gamee:
                 if data is None:
                     headers["content-length"] = "0"
                     res = requests.get(url, headers=headers)
-                    open("http.log", "a", encoding="utf-8").write(
-                        res.text + "\n"
-                    )
+                    open("http.log", "a", encoding="utf-8").write(res.text + "\n")
                     if "<title>" in res.text:
                         time.sleep(1)
                         self.log(f"{kuning}failed get json response !")
@@ -70,8 +68,7 @@ class Gamee:
             jam = str(jam).zfill(2)
             menit = str(menit).zfill(2)
             detik = str(detik).zfill(2)
-            print(f"waiting until {jam}:{menit}:{detik} ",
-                  flush=True, end="\r")
+            print(f"waiting until {jam}:{menit}:{detik} ", flush=True, end="\r")
             t -= 1
             time.sleep(1)
         print("                          ", flush=True, end="\r")
@@ -82,13 +79,12 @@ class Gamee:
     def load_config(self, config_file="config.json"):
         try:
             config = json.loads(open(config_file, "r").read())
-            self.DEFAULT_COUNTDOWN = config['countdown']
-            self.DEFAULT_INTERVAL = config['interval']
-            self.USE_TICKET_TO_SPIN = config['use_ticket_to_spin']
-            self.MAX_USE_TICKET = config['max_use_ticket_to_spin']
+            self.DEFAULT_COUNTDOWN = config["countdown"]
+            self.DEFAULT_INTERVAL = config["interval"]
+            self.USE_TICKET_TO_SPIN = config["use_ticket_to_spin"]
+            self.MAX_USE_TICKET = config["max_use_ticket_to_spin"]
         except json.decoder.JSONDecodeError:
-            self.log(
-                f'{merah}failed to get you config, please check {config_file}')
+            self.log(f"{merah}failed to get you config, please check {config_file}")
             sys.exit()
 
     def gamee_login(self, tg_data, uuid):
@@ -156,63 +152,65 @@ class Gamee:
         )
         try:
             res = self.http(self.url_api_gamee, headers, daily_get_price)
-            daily_spin = res.json()[
-                "result"]["dailyReward"]["spinsCountAvailable"]
-            spin_using_ticket_price = res.json(
-            )['result']['dailyReward']['dailyRewardBonusSpinsPriceTickets']
-            tickets = res.json()['user']['tickets']['count']
-            self.log(f'{hijau}available ticket : {putih}{tickets}')
-            self.log(f'{hijau}available free spin : {putih}{daily_spin}')
+            daily_spin = res.json()["result"]["dailyReward"]["spinsCountAvailable"]
+            spin_using_ticket_price = res.json()["result"]["dailyReward"][
+                "dailyRewardBonusSpinsPriceTickets"
+            ]
+            tickets = res.json()["user"]["tickets"]["count"]
+            self.log(f"{hijau}available ticket : {putih}{tickets}")
+            self.log(f"{hijau}available free spin : {putih}{daily_spin}")
             self.log(
-                f'{hijau}price to spin : {putih}{spin_using_ticket_price} {hijau}ticket')
+                f"{hijau}price to spin : {putih}{spin_using_ticket_price} {hijau}ticket"
+            )
             if daily_spin > 0:
                 for i in range(daily_spin):
-                    res = self.http(self.url_api_gamee, headers,
-                                    daily_reward_claim_prize)
+                    res = self.http(
+                        self.url_api_gamee, headers, daily_reward_claim_prize
+                    )
                     reward_type = res.json()["result"]["reward"]["type"]
                     if reward_type == "money":
                         key = "usdCents"
                     else:
                         key = reward_type
                     reward = res.json()["result"]["reward"][key]
-                    self.log(
-                        f"{hijau}reward spin : {putih}{reward} {reward_type}")
+                    self.log(f"{hijau}reward spin : {putih}{reward} {reward_type}")
 
             if self.USE_TICKET_TO_SPIN:
-                self.log(f'{biru}start spin using ticket !')
+                self.log(f"{biru}start spin using ticket !")
                 while True:
                     if tickets < spin_using_ticket_price:
-                        self.log(f'{kuning}not enough tickets for spin !')
+                        self.log(f"{kuning}not enough tickets for spin !")
                         return
 
                     if spin_using_ticket_price > self.MAX_USE_TICKET:
-                        self.log(f'{kuning}max using ticket to spin reacted !')
+                        self.log(f"{kuning}max using ticket to spin reacted !")
                         return
 
-                    res = self.http(self.url_api_gamee, headers,
-                                    buy_spin_using_ticket)
-                    res = self.http(self.url_api_gamee, headers,
-                                    daily_reward_claim_prize)
+                    res = self.http(self.url_api_gamee, headers, buy_spin_using_ticket)
+                    res = self.http(
+                        self.url_api_gamee, headers, daily_reward_claim_prize
+                    )
                     reward_type = res.json()["result"]["reward"]["type"]
                     if reward_type == "money":
                         key = "usdCents"
                     else:
                         key = reward_type
                     reward = res.json()["result"]["reward"][key]
+                    self.log(f"{hijau}reward spin : {putih}{reward} {reward_type}")
+                    res = self.http(self.url_api_gamee, headers, daily_get_price)
+                    daily_spin = res.json()["result"]["dailyReward"][
+                        "spinsCountAvailable"
+                    ]
+                    spin_using_ticket_price = res.json()["result"]["dailyReward"][
+                        "dailyRewardBonusSpinsPriceTickets"
+                    ]
+                    tickets = res.json()["user"]["tickets"]["count"]
+                    self.log(f"{hijau}available ticket : {putih}{tickets}")
                     self.log(
-                        f"{hijau}reward spin : {putih}{reward} {reward_type}")
-                    res = self.http(self.url_api_gamee,
-                                    headers, daily_get_price)
-                    daily_spin = res.json()[
-                        "result"]["dailyReward"]["spinsCountAvailable"]
-                    spin_using_ticket_price = res.json(
-                    )['result']['dailyReward']['dailyRewardBonusSpinsPriceTickets']
-                    tickets = res.json()['user']['tickets']['count']
-                    self.log(f'{hijau}available ticket : {putih}{tickets}')
-                    self.log(
-                        f'{hijau}price to spin : {putih}{spin_using_ticket_price} {hijau}ticket')
+                        f"{hijau}price to spin : {putih}{spin_using_ticket_price} {hijau}ticket"
+                    )
         except KeyError as e:
-            self.log(f'{merah}something error, {e}')
+            self.log(f"{merah}something error, {e}")
             return False
 
     def gamee_mining_page(self, access_token, uuid):
@@ -231,14 +229,14 @@ class Gamee:
             "id": "miningEvent.get",
             "method": "miningEvent.get",
             "params": {
-                "miningEventId": 11,
+                "miningEventId": 12,
             },
         }
         data_start_mining = {
             "jsonrpc": "2.0",
             "id": "miningEvent.startSession",
             "method": "miningEvent.startSession",
-            "params": {"miningEventId": 11, "code": "watprotocol"},
+            "params": {"miningEventId": 12, "code": "WATPROTOCOL"},
         }
         res = self.http(self.url_api_gamee, headers, json.dumps(data))
         assets = res.json()["user"]["assets"]
@@ -277,7 +275,7 @@ class Gamee:
                 if "error" in res.json().keys():
                     msg = res.json()["error"]["message"].lower()
                     if msg == "mining session in progress.":
-                        self.log(f'{kuning}mining in progress')
+                        self.log(f"{kuning}mining in progress")
                         return
                     time.sleep(2)
                     continue
@@ -311,11 +309,17 @@ class Gamee:
     
         """
         arg = argparse.ArgumentParser()
-        arg.add_argument('--marinkitagawa', action="store_true")
+        arg.add_argument("--marinkitagawa", action="store_true")
         arg.add_argument(
-            '--data', help="set custom file input data (default: data.txt)", default="data.txt")
+            "--data",
+            help="set custom file input data (default: data.txt)",
+            default="data.txt",
+        )
         arg.add_argument(
-            '--config', help="set custom config (default: config.json)", default="config.json")
+            "--config",
+            help="set custom config (default: config.json)",
+            default="config.json",
+        )
         args = arg.parse_args()
         if args.marinkitagawa is False:
             os.system("cls" if os.name == "nt" else "clear")
@@ -324,7 +328,8 @@ class Gamee:
 
         if not os.path.exists(args.data):
             self.log(
-                f"{merah} {args.data} is not found, input valid path file please !")
+                f"{merah} {args.data} is not found, input valid path file please !"
+            )
             sys.exit()
 
         self.load_config(args.config)
@@ -341,8 +346,8 @@ class Gamee:
                 data_parse = self.data_parsing(account)
                 user = json.loads(data_parse["user"])
                 userid = user["id"]
-                first_name = user['first_name']
-                self.log(f'{hijau}login as {putih}{first_name}')
+                first_name = user["first_name"]
+                self.log(f"{hijau}login as {putih}{first_name}")
                 data = f"query_id={data_parse['query_id']}&user={quote(data_parse['user'])}&auth_date={data_parse['auth_date']}&hash={data_parse['hash']}"
                 uids = json.loads(open("uuid.json", "r").read())
                 if str(userid) not in uids.keys():
